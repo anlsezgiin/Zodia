a8e27772ef88e07f42852dc97f29d2877fa6c5d7
anlsezgiin <anlsezgin3@gmail.com>
2025-08-25 09:34:39 +0300

    checkAuth added

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -99,3 +99,12 @@ export const updateProfile = async (req,res) => {
         res.status(500).json({message: "Server hatası!"})
     }
 }
+
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("checkAuth controller'ında hata!", error.message);
+        res.status(500).json({ message: "Server hatası!" });
+  }
+}
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup, login, logout, updateProfile } from "../controllers/auth.controller.js";
+import { signup, login, logout, updateProfile, checkAuth } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post("/login",login);
 router.post("/signup", signup);
 router.post("/logout", logout);
 router.put("/updateProfile", protectRoute, updateProfile)
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
