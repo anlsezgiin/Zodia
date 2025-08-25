7ab25df362e80c041b2b009df8cc053570478f5d
anlsezgiin <anlsezgin3@gmail.com>
2025-08-25 09:14:58 +0300

    little updates

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -68,7 +68,7 @@ export const login = async (req,res) => {
     }
 };
 
-export const logout = (req, res) => {
+export const logout = (res) => {
   try {
     res.cookie("jwt", "", { maxAge: 0 });
     res.status(200).json({ message: "Başarıyla çıkış yapıldı" });
